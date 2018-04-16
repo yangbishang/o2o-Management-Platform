35769a33b2db54a1e980c06aed23576bf7d45c61
yangbishang <774356294@qq.com>
2018-04-16 23:31:31 +0800

    add some pojo class

---
+++ o2o/src/main/java/com/yy/dao/pojo/Area.java
@@ -0,0 +1,51 @@
+package com.yy.dao.pojo;
+
+import java.util.Date;
+
+public class Area {
+    private Integer areaId; //ID
+    private String areaName;//名称
+    private String priority;//权重
+    private Date createTime;//创建时间
+    private Date lastEditTime;//更新时间
+
+    public Integer getAreaId() {
+        return areaId;
+    }
+
+    public void setAreaId(Integer areaId) {
+        this.areaId = areaId;
+    }
+
+    public String getAreaName() {
+        return areaName;
+    }
+
+    public void setAreaName(String areaName) {
+        this.areaName = areaName;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getLastEditTime() {
+        return lastEditTime;
+    }
+
+    public void setLastEditTime(Date lastEditTime) {
+        this.lastEditTime = lastEditTime;
+    }
+}
